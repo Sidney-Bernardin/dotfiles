e78210f302093ac5d76b3dad967fc952ce8c6410
Sidney Bernardin <sidney.bernardin@gmail.com>
2022-09-13 10:26:49 -0400

    Added harpoon keymaps.

--- .config/nvim/lua/sidney/packer.lua
+++ .config/nvim/lua/sidney/packer.lua
@@ -15,7 +15,10 @@ return require('packer').startup(function(use)
     use 'L3MON4D3/LuaSnip'
     use 'saadparwaiz1/cmp_luasnip'
 
-    -- Telescope
+    -- Plenary
+    use 'nvim-lua/plenary.nvim'
+
+    -- Telescope    
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
         requires = { {'nvim-lua/plenary.nvim'} }
@@ -34,4 +37,8 @@ return require('packer').startup(function(use)
     use 'windwp/nvim-autopairs'
     use 'unblevable/quick-scope'
     use 'machakann/vim-highlightedyank'
+    use {
+        'ThePrimeagen/harpoon',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 end)
