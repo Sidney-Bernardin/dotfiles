c36ad98eb356142b550c0270f5e556ab6103db71
Sidney Bernardin <sidney.bernardin@gmail.com>
2022-09-16 22:08:34 -0400

    Changed harpoon keymaps.open-project-in-tmux

--- .config/nvim/after/plugin/plugins/harpoon.lua
+++ .config/nvim/after/plugin/plugins/harpoon.lua
@@ -1,13 +1,14 @@
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true }
 
--- Add file.
-keymap('n', '<C-a>', ':lua require("harpoon.mark").add_file()<CR>', opts)
+-- Mark file.
+keymap('n', '<A-m>', ':lua require("harpoon.mark").add_file()<CR>', opts)
 
 -- Open menu.
-keymap('n', '<C-u>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', opts)
+keymap('n', '<A-u>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', opts)
 
 -- File navigation.
-keymap('n', '<C-j>', ':lua require("harpoon.ui").nav_file(1)<CR>', opts)
-keymap('n', '<C-k>', ':lua require("harpoon.ui").nav_file(2)<CR>', opts)
-keymap('n', '<C-l>', ':lua require("harpoon.ui").nav_file(3)<CR>', opts)
+keymap('n', '<A-j>', ':lua require("harpoon.ui").nav_file(1)<CR>', opts)
+keymap('n', '<A-k>', ':lua require("harpoon.ui").nav_file(2)<CR>', opts)
+keymap('n', '<A-l>', ':lua require("harpoon.ui").nav_file(3)<CR>', opts)
+keymap('n', '<A-;>', ':lua require("harpoon.ui").nav_file(4)<CR>', opts)
--- .profile
+++ .profile
@@ -31,11 +31,3 @@ export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:$CARGO
 VERSION=v16.17.0
 DISTRO=linux-x64
 export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH
-
-# FZF env.
-export FZF_DEFAULT_COMMAND="find * -type f -not -path '*/\.*'"
-export FZF_DEFAULT_OPTS="--no-height"
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_CTRL_T_OPTS="--preview 'batcat --color=always --line-range :50 {}'"
-export FZF_ALT_C_COMMAND="find * -type d -not -path '*/\.*'"
-export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -50'"
