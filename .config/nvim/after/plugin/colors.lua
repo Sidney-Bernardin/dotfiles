994e9b0f20de3bb4419b9eaddf28dfb64af40928
Sidney Bernardin <sidney.bernardin@gmail.com>
2022-12-12 18:21:56 -0500

    Updated some file names and changed Telescope colors a bit.

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,50 +1,54 @@
 local hl = vim.api.nvim_set_hl
 
--- Gruvbox!
-vim.cmd('colorscheme gruvbox')
+local function gruvbox_minimal()
 
--- Highlighted Yank
-hl(0, 'HighlightYank', { bold = true, ctermfg = 235, ctermbg = 214 })
+    vim.cmd('colorscheme gruvbox')
 
--- Cursor Line
-vim.cmd([[ highlight! link Operator GruvboxFg1 ]]) -- To fix Gruvbox CursorLine bug.
-hl(0, 'CursorLine', {})
-hl(0, 'CursorLineNr', { ctermbg = 235, ctermfg = 214 })
+    -- Highlighted Yank
+    hl(0, 'HighlightYank', { bold = true, ctermfg = 235, ctermbg = 214 })
 
--- Treesitter Context
-hl(0, 'TreesitterContext', { ctermbg = 237 })
-hl(0, 'TreesitterContextLineNumber', { ctermbg = 237, ctermfg = 214 })
+    -- Cursor Line
+    vim.cmd([[ highlight! link Operator GruvboxFg1 ]]) -- To fix Gruvbox CursorLine bug.
+    hl(0, 'CursorLine', {})
+    hl(0, 'CursorLineNr', { ctermbg = 235, ctermfg = 214 })
 
--- Telescope
-hl(0, 'TelescopeMatching', { ctermfg = 214 })
-hl(0, 'TelescopePromptTitle', { ctermfg = 223 })
-hl(0, 'TelescopePromptNormal', { ctermbg = 239 })
-hl(0, 'TelescopePromptBorder', { ctermbg = 239, ctermfg = 239 })
-hl(0, 'TelescopePromptPrefix', { ctermfg = 214 })
-hl(0, 'TelescopePromptCounter', { ctermfg = 214 })
-hl(0, 'TelescopeResultsTitle', { ctermfg = 238 })
-hl(0, 'TelescopeResultsNormal', { ctermbg = 238, ctermfg = 223 })
-hl(0, 'TelescopeResultsBorder', { ctermbg = 238, ctermfg = 238 })
-hl(0, 'TelescopeSelection', { ctermbg = 239, ctermfg = 223 })
-hl(0, 'TelescopePreviewTitle', { ctermfg = 223 })
-hl(0, 'TelescopePreviewNormal', { ctermbg = 237 })
-hl(0, 'TelescopePreviewBorder', { ctermbg = 237, ctermfg = 237 })
+    -- Treesitter Context
+    hl(0, 'TreesitterContext', { ctermbg = 237 })
+    hl(0, 'TreesitterContextLineNumber', { ctermbg = 237, ctermfg = 214 })
 
--- Eyeliner
-hl(0, 'EyelinerPrimary', { ctermfg = 175 })
-hl(0, 'EyelinerSecondary', { ctermfg = 132 })
+    -- Telescope
+    hl(0, 'TelescopeMatching', { ctermfg = 214 })
+    hl(0, 'TelescopePromptTitle', { ctermfg = 223 })
+    hl(0, 'TelescopePromptNormal', { ctermbg = 239 })
+    hl(0, 'TelescopePromptBorder', { ctermbg = 239, ctermfg = 239 })
+    hl(0, 'TelescopePromptPrefix', { ctermfg = 214 })
+    hl(0, 'TelescopePromptCounter', { ctermfg = 214 })
+    hl(0, 'TelescopeResultsTitle', { ctermfg = 238 })
+    hl(0, 'TelescopeResultsNormal', { ctermbg = 238, ctermfg = 223 })
+    hl(0, 'TelescopeResultsBorder', { ctermbg = 238, ctermfg = 238 })
+    hl(0, 'TelescopeSelection', { ctermbg = 240, ctermfg = 223 })
+    hl(0, 'TelescopePreviewTitle', { ctermfg = 237 })
+    hl(0, 'TelescopePreviewNormal', { ctermbg = 237 })
+    hl(0, 'TelescopePreviewBorder', { ctermbg = 237, ctermfg = 237 })
 
--- Harpoon
-hl(0, 'HarpoonWindow', { ctermbg = 239 })
-hl(0, 'HarpoonBorder', { ctermbg = 239, ctermfg = 223 })
+    -- Eyeliner
+    hl(0, 'EyelinerPrimary', { ctermfg = 175 })
+    hl(0, 'EyelinerSecondary', { ctermfg = 132 })
 
--- Indent Blankline
-hl(0, 'IndentBlanklineChar', { ctermfg = 236 })
+    -- Harpoon
+    hl(0, 'HarpoonWindow', { ctermbg = 239 })
+    hl(0, 'HarpoonBorder', { ctermbg = 239, ctermfg = 223 })
 
--- Other
-hl(0, 'Visual', { bold = true, ctermbg = 109, ctermfg = 235 })
-hl(0, 'ColorColumn', { ctermbg = 236 })
-hl(0, 'WinBar', { ctermfg = 250 })
-hl(0, 'SignColumn', { ctermbg = 235 })
-hl(0, 'QuickFixLine', { ctermbg = 237 })
-hl(0, 'FloatBorder', { ctermbg = 239, ctermfg = 239 })
+    -- Indent Blankline
+    hl(0, 'IndentBlanklineChar', { ctermfg = 236 })
+
+    -- Other
+    hl(0, 'Visual', { bold = true, ctermbg = 109, ctermfg = 235 })
+    hl(0, 'ColorColumn', { ctermbg = 236 })
+    hl(0, 'WinBar', { ctermfg = 250 })
+    hl(0, 'SignColumn', { ctermbg = 235 })
+    hl(0, 'QuickFixLine', { ctermbg = 237 })
+    hl(0, 'FloatBorder', { ctermbg = 239, ctermfg = 239 })
+end
+
+gruvbox_minimal()
--- .config/nvim/lua/sidney/plugins/telescope.lua
+++ .config/nvim/lua/sidney/plugins/telescope.lua
@@ -2,6 +2,9 @@ local telescope = require('telescope')
 
 telescope.setup({
     defaults = {
+        selection_caret = ' ',
+        entry_prefix = ' ',
+        prompt_prefix = ' ',
         sorting_strategy = 'ascending',
         layout_strategy = 'horizontal',
         layout_config = {
